d5d33cac8673e7586badb50cd7d07ec79176659c
raphael <moudens.raphael@orange.fr>
2017-03-20 14:51:19 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercice php</title>
+</head>
+<body>
+	<?php echo $num = 0;
+	for($i = 1; $i <=10; $i++){
+		echo $i;
+	} 
+	?>
+</body>
+</html>
---
+++ loop/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercice php</title>
+	<?php echo "ceci est du php"; ?>
+</head>
+<body>
+	
+</body>
+</html>
