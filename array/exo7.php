ca9adcba05a4bd9d5781e35d765a0697d67219c5
raphael <moudens.raphael@orange.fr>
2017-03-21 16:41:15 +0100

    exo7 ok

---
+++ array/exo7.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo7</title>
+</head>
+<body>
+	<?php 
+	$hautDeFrance = array(59 => "Nord", 62 => "Pas de calais", 80 => "Somme", 02 => "Aisne", 60 => "Oise");
+	echo $hautDeFrance[51] = "Marne";
+	?>
+</body>
+</html>
