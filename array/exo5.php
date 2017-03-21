51163e3fe1f8fc848957b8f8449a6a014cea4146
raphael <moudens.raphael@orange.fr>
2017-03-21 16:27:21 +0100

    exo5&6 ok

---
+++ array/exo5.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo5 et 6</title>
+</head>
+<body>
+	<?php 
+	$hautDeFrance = array(59 => "Nord", 62 => "Pas de calais", 80 => "Somme", 02 => "Aisne", 60 => "Oise");
+	echo $hautDeFrance[59];
+	?>
+</body>
+</html>
