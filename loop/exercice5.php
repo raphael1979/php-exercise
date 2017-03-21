dcab546063661360e6f7936875101c68297498d0
raphael <moudens.raphael@orange.fr>
2017-03-21 11:01:31 +0100

    exo 5 ok

---
+++ loop/exercice5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo5</title>
+</head>
+<body>
+	<?php 
+	for($i = 1 ; $i <= 15 ; $i++){
+		echo($i);
+		echo (' ');
+		echo ('On y arrive presque');
+		echo (' ');
+	}
+	?>
+</body>
+</html>
