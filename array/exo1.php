e646fc231779afa88b922697458963620750b43e
raphael <moudens.raphael@orange.fr>
2017-03-21 15:45:45 +0100

    exo1 ok

---
+++ array/exo1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo 1</title>
+</head>
+<body>
+	<?php 
+	$mois = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"];
+	echo $mois;
+	?>
+</body>
+</html>
