70fb57c0d34170924e2d768828caef984b427b2c
raphael <moudens.raphael@orange.fr>
2017-03-21 14:00:27 +0100

    exo2 ok

---
+++ function/exo2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo2</title>
+</head>
+<body>
+	<?php 
+	function returnString($string){
+		return $string;
+	}
+	echo returnString("Raphael");
+
+	?>
+	
+</body>
+</html>
