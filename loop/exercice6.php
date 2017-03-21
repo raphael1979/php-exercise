b192dd76006ffc8cd5b390a3dc147260c6f5069f
raphael <moudens.raphael@orange.fr>
2017-03-21 11:11:40 +0100

    exo 6 ok

---
+++ loop/exercice6.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo6</title>
+</head>
+<body>
+	<?php 
+	for($i = 20 ; $i >= 0 ; --$i){
+		echo($i);
+		echo (' ');
+		echo ("C'est presque bon");
+		echo (' ');
+	}
+	?>
+</body>
+</html>
