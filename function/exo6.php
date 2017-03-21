2fcaffc8c2f4e5f5e6f7517481e005a51076719b
raphael <moudens.raphael@orange.fr>
2017-03-21 14:39:41 +0100

    exo6 ok

---
+++ function/exo6.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo 6</title>
+</head>
+<body>
+	<?php 
+	function civile ($nom, $prenom, $age){
+		return "Bonjour ".$nom.$prenom." tu as ".$age." ans";
+	}
+	echo civile("Moudens ", " raphael", 38);
+	?>
+	
+</body>
+</html>
