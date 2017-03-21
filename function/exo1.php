4b36bd829ad34f173d2f7659525e424d88297a0d
raphael <moudens.raphael@orange.fr>
2017-03-21 13:45:40 +0100

    exo1 ok

---
+++ function/exo1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo1</title>
+
+</head>
+<body>
+	<?php 
+	function vrai(){
+		return true;
+	}	
+	echo vrai();
+	?>	
+</body>
+</html>
