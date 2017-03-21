235f7fb3b9d5716dad057cad78287b150ff2dca0
raphael <moudens.raphael@orange.fr>
2017-03-21 11:22:03 +0100

    exo7 ok

---
+++ loop/exercice7.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo 7</title>
+</head>
+<body>
+	<?php 
+	for ($i = 1 ; $i < 100 ; $i+=15){
+		echo($i);
+		echo (' ');
+		echo ('On tient le bon bout !!');
+		echo (' ');
+	}
+	?>
+
+</body>
+</html>
