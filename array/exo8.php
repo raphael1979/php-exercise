527f2b7b42ddaf915c9265cc3ff5e5d0564f5659
raphael <moudens.raphael@orange.fr>
2017-03-21 16:50:34 +0100

    exo8 ok

---
+++ array/exo8.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo 8</title>
+</head>
+<body>
+	<?php 
+	$mois = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"];
+	foreach ($mois as $moa ) {
+		echo $moa." ";
+	}
+	?>
+</body>
+</html>
