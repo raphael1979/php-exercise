381b87040ba1f188976587850251a90388fc7a5c
raphael <moudens.raphael@orange.fr>
2017-03-21 14:07:19 +0100

    exo3 ok

---
+++ function/exo3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo3</title>
+</head>
+<body>
+	<?php 
+	function stringAdd($string1, $string2){
+		return $string1.$string2;
+	}
+	echo stringAdd("C'est pas faux", " en tant que tel");
+
+	?>
+	
+</body>
+</html>
