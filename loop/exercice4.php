d3fc64560d2fc77c096d6e36ee3ec195993bc181
raphael <moudens.raphael@orange.fr>
2017-03-21 10:45:42 +0100

    exo4 ok

---
+++ loop/exercice4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo 4</title>
+</head>
+<body>
+
+	<?php
+	$a = 1;
+	$b = $a/2;
+	while($a < 10){
+		echo($a);
+		$a = $a+$b;
+		echo(' ');
+	}
+	?>
+</body>
+</html>
