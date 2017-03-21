5e66d01da12ed017f66af9af69599f089cf5b9ce
raphael <moudens.raphael@orange.fr>
2017-03-21 15:14:31 +0100

    exo 8 ok

---
+++ function/exo8.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo 8</title>
+</head>
+<body>
+	<?php 
+	function nbr($nb1 = 3, $nb2 = 4, $nb3 = 2){
+		return $nb1 + $nb2 + $nb3;
+	}
+	echo nbr();
+	?>
+	
+</body>
+</html>
