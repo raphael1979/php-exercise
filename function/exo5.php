071ee392e43ee6610ee73ae691522e2153aedb3e
raphael <moudens.raphael@orange.fr>
2017-03-21 14:27:42 +0100

    exo5 ok

---
+++ function/exo5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo 5</title>
+</head>
+<body>
+	<?php 
+	function stringAdd($string, $nbr){
+		return $string.$nbr;
+	}
+	echo stringAdd("C'est pas faux", 21);
+	?>
+	
+</body>
+</html>
