949e6c3ce8ce8a90b2cef17bfe3c39f220f13794
raphael <moudens.raphael@orange.fr>
2017-03-21 11:27:35 +0100

    exo8 ok loop ok

---
+++ loop/exercice8.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo 7</title>
+</head>
+<body>
+	<?php 
+	for ($i = 200 ; $i > 0 ; $i-=12){
+		echo($i);
+		echo (' ');
+		echo ('Enfin');
+		echo (' ');
+	}
+	?>
+
+</body>
+</html>
