7a88249a4975eb1296b90e6be9db2c488f7a1c0e
raphael <moudens.raphael@orange.fr>
2017-03-22 14:20:13 +0100

    exo1.php

---
+++ form/exo1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>formulaire1</title>
+</head>
+<body>
+	<form action="user.php" method="GET">
+		<label for="name">NOM</label>
+		<input type="text" name="name">
+		<label for="name">Prenom</label>
+		<input type="text" name="firstName">
+		<input type="submit" value="Envoyer">
+	</form>
+	<?php 
+	
+	?>
+</body>
+</html>
