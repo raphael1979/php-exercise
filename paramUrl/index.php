6575b2a6199ea32d7a977ae4658d65da3cbb8c46
raphael <moudens.raphael@orange.fr>
2017-03-22 13:55:54 +0100

    exo1 param url ok

---
+++ paramUrl/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo1</title>
+</head>
+<body>
+	<?php 
+
+	echo $_GET['prenom']." ";
+	echo $_GET['nom']." ";
+
+	?>
+	
+</body>
+</html>
