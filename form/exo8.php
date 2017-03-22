568a3b75b1b829343dc0d0fc4d103fb0687a5e30
raphael <moudens.raphael@orange.fr>
2017-03-22 16:51:13 +0100

    exo8 ok

---
+++ form/exo8.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>formulaire8</title>
+</head>
+<body>
+	<?php
+	if (!$_POST["civilit"]||!$_POST["name"]||!$_POST["first_name"]) {
+
+		echo '<form action="index.php" method="POST">
+		<select name="civilit">
+			<option value="Mr">Mr</option>
+			<option value="Mme">Mme</option>
+		</select>
+		<label for="name">NOM</label>
+		<input type="text" name="name">
+		<label for="name">Prenom</label>
+		<input type="text" name="first_name">
+		<label for="name">Fichier</label>
+		<input type="file" name="file">
+		<input type="submit" value="Envoyer"></form>';
+	}else{
+		echo $_POST["civilit"] . " " . $_POST["name"] . " " . $_POST["first_name"]." ".$_POST["file"];
+		if(explode(".", $_POST["file"])[1]==="pdf"){
+			echo "C'est bien un pdf";
+		}
+	}
+	?>
+</body>
+</html>
