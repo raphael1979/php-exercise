2244f7daca707b79ca54def8b702e1012a366ab7
raphael <moudens.raphael@orange.fr>
2017-03-23 15:16:53 +0100

    exo3 et 4

--- GlobalVariable/exercice3/index.php
+++ GlobalVariable/exercice3/index.php
@@ -2,11 +2,23 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>GlobalVariable</title>
+	<title>exo3</title>
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<?php 
-	?>
-</body>
-</html>
+	<form action="inscription.php" method="POST">
+		<p><label for="">Login :</label>
+			<input type="text" name="login" value="raf"></p>
+			<p><label for="">Mot de passe :</label>
+				<input type="password" name="mdp" value="123"></p>
+				<p><input type="submit" value="connection">
+					<label for="">Envoyer</label></p>
+				</form>
+				<?php 
+				
+				setcookie("login", $_POST["login"]);
+				setcookie("mdp", $_POST["mdp"]);
+
+				?>
+			</body>
+			</html>
---
+++ GlobalVariable/exercice3/inscription.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>inscription.php</title>
+</head>
+<body>
+	<?php 
+	echo $_COOKIE['login']. " " .$_COOKIE['mdp'];
+	
+	?>
+</body>
+</html>
