dff99ac0ae0ba06132dd2107b55edcaaa740c243
raphael <moudens.raphael@orange.fr>
2017-03-24 15:39:24 +0100

    exo6 ok

---
+++ paramUrl/index6.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo6</title>
+</head>
+<body>
+	<?php 
+	echo  $_GET["batiment"]." ";
+	echo  $_GET["salle"]." ";
+	?>
+	
+</body>
+</html>
