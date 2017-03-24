d8c47fb08db348523d39b338c3ffa22f9ac2161a
raphael <moudens.raphael@orange.fr>
2017-03-24 15:26:59 +0100

    exo3 ok

---
+++ paramUrl/index3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo3</title>
+</head>
+<body>
+	<?php 
+	echo $_GET["dateDebut"]." ";
+	echo $_GET["dateFin"]." ";
+	?>
+	
+</body>
+</html>
