9c0c566ec475a89ea894c3c75f85c40b93a021d4
raphael <moudens.raphael@orange.fr>
2017-03-24 15:33:52 +0100

    exo5 ok

---
+++ paramUrl/index4.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo4</title>
+</head>
+<body>
+	<?php 
+	echo $_GET["langage"]." ";
+	echo $_GET["serveur"]." ";
+	?>
+	
+</body>
+</html>
---
+++ paramUrl/index5.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo5</title>
+</head>
+<body>
+	<?php 
+	echo $_GET["semaine"]." ";
+	?>
+</body>
+</html>
